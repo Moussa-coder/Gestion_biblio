16b99cac9f9ba2114a10b327403efd799e9e44ba
Moussa <correamoussa469@gmail.com>
2025-06-26 22:07:31 +0000

    Initialization de mon prohet angular

---
+++ frontend-bibliotheque/src/app/livres/livre-details/livre-details.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-livre-details',
+  imports: [],
+  templateUrl: './livre-details.component.html',
+  styleUrl: './livre-details.component.css'
+})
+export class LivreDetailsComponent {
+
+}
---
+++ frontend-bibliotheque/src/app/livres/livres-list/livres-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-livres-list',
+  imports: [],
+  templateUrl: './livres-list.component.html',
+  styleUrl: './livres-list.component.css'
+})
+export class LivresListComponent {
+
+}
